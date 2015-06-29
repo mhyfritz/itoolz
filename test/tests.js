4d8f6fccd9cca64f9cb9e45a61dc13dfb76930a4
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-06-29 10:06:34 +0200

    Rename variables

--- test/tests.js
+++ test/tests.js
@@ -3,60 +3,60 @@ let test = require('tape');
 
 test('accumulate - default func - list', function (t) {
   t.plan(1);
-  let result, expect;
-  result = itoolz.accumulate([1, 2, 3, 4, 5]);
-  result = [...result];
+  let actual, expect;
+  actual = itoolz.accumulate([1, 2, 3, 4, 5]);
+  actual = [...actual];
   expect = [1, 3, 6, 10, 15];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('accumulate - default func - generator', function (t) {
   t.plan(1);
-  let result, expect;
-  result = itoolz.accumulate(function* () {
+  let actual, expect;
+  actual = itoolz.accumulate(function* () {
     for (let x of [1, 2, 3, 4, 5]) {
       yield x;
     }
   }());
-  result = [...result];
+  actual = [...actual];
   expect = [1, 3, 6, 10, 15];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('accumulate - multiply func - list', function (t) {
   t.plan(1);
-  let result, expect;
-  result = itoolz.accumulate([1, 2, 3, 4, 5], (x, y) => x * y);
-  result = [...result];
+  let actual, expect;
+  actual = itoolz.accumulate([1, 2, 3, 4, 5], (x, y) => x * y);
+  actual = [...actual];
   expect = [1, 2, 6, 24, 120];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('accumulate - multiply func - generator', function (t) {
   t.plan(1);
-  let result, expect;
-  result = itoolz.accumulate(function* () {
+  let actual, expect;
+  actual = itoolz.accumulate(function* () {
     for (let x of [1, 2, 3, 4, 5]) {
       yield x;
     }
   }(), (x, y) => x * y);
-  result = [...result];
+  actual = [...actual];
   expect = [1, 2, 6, 24, 120];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('chain - two seqs - list', function (t) {
   t.plan(1);
-  let result, expect;
-  result = itoolz.chain('ABC', 'DEF');
-  result = [...result];
+  let actual, expect;
+  actual = itoolz.chain('ABC', 'DEF');
+  actual = [...actual];
   expect = ['A', 'B', 'C', 'D', 'E', 'F'];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('chain - two seqs - generator', function (t) {
   t.plan(1);
-  let result, expect;
+  let actual, expect;
   function* g() {
     for (let x of 'ABC') {
       yield x;
@@ -67,24 +67,24 @@ test('chain - two seqs - generator', function (t) {
       yield x;
     }
   }
-  result = itoolz.chain(g(), h());
-  result = [...result];
+  actual = itoolz.chain(g(), h());
+  actual = [...actual];
   expect = ['A', 'B', 'C', 'D', 'E', 'F'];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('chainFromIterable - two seqs - list', function (t) {
   t.plan(1);
-  let result, expect;
-  result = itoolz.chainFromIterable(['ABC', 'DEF']);
-  result = [...result];
+  let actual, expect;
+  actual = itoolz.chainFromIterable(['ABC', 'DEF']);
+  actual = [...actual];
   expect = ['A', 'B', 'C', 'D', 'E', 'F'];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('chainFromIterable - two seqs - generator', function (t) {
   t.plan(1);
-  let result, expect;
+  let actual, expect;
   function* g() {
     for (let x of 'ABC') {
       yield x;
@@ -95,31 +95,31 @@ test('chainFromIterable - two seqs - generator', function (t) {
       yield x;
     }
   }
-  result = itoolz.chainFromIterable([g(), h()]);
-  result = [...result];
+  actual = itoolz.chainFromIterable([g(), h()]);
+  actual = [...actual];
   expect = ['A', 'B', 'C', 'D', 'E', 'F'];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('compress - string', function (t) {
   t.plan(1);
-  let result, expect;
-  result = itoolz.compress('ABCDEF', [1, 0, 1, 0, 1, 1]);
-  result = [...result];
+  let actual, expect;
+  actual = itoolz.compress('ABCDEF', [1, 0, 1, 0, 1, 1]);
+  actual = [...actual];
   expect = ['A', 'C', 'E', 'F'];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
 
 test('compress - string / generator', function (t) {
   t.plan(1);
-  let result, expect;
+  let actual, expect;
   function* g() {
     for (let x of 'ABCDEF') {
       yield x;
     }
   }
-  result = itoolz.compress(g(), [1, 0, 1, 0, 1, 1]);
-  result = [...result];
+  actual = itoolz.compress(g(), [1, 0, 1, 0, 1, 1]);
+  actual = [...actual];
   expect = ['A', 'C', 'E', 'F'];
-  t.deepEqual(result, expect);
+  t.deepEqual(actual, expect);
 });
