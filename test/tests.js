5d083b0237c4f407659053ac829f9a5ff826b7ec
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-06-29 08:36:08 +0200

    Add tests for `chainFromIterable`

--- test/tests.js
+++ test/tests.js
@@ -52,3 +52,19 @@ test('chain - two seqs - generator', function (t) {
   res = [...res];
   t.deepEqual(res, ['A', 'B', 'C', 'D', 'E', 'F']);
 });
+
+test('chainFromIterable - two seqs - list', function (t) {
+  t.plan(1);
+  var res = itoolz.chainFromIterable(['ABC', 'DEF']);
+  res = [...res];
+  t.deepEqual(res, ['A', 'B', 'C', 'D', 'E', 'F']);
+});
+
+test('chainFromIterable - two seqs - generator', function (t) {
+  t.plan(1);
+  function* g() { for (let x of 'ABC') { yield x; } };
+  function* h() { for (let x of 'DEF') { yield x; } };
+  var res = itoolz.chainFromIterable([g(), h()]);
+  res = [...res];
+  t.deepEqual(res, ['A', 'B', 'C', 'D', 'E', 'F']);
+});
